558c1fd3ac2c9999895bc97475dbcef0cee8d51c
kevish-gawri-vinci <kevish.gawri@student.vinci.be>
2023-12-12 14:01:32 +0100

    test deploy 2

--- src/Components/Pages/ProfilPage.js
+++ src/Components/Pages/ProfilPage.js
@@ -1,7 +1,8 @@
 
 import { getUserSessionData } from '../../utils/auth';
 
-const { username, email, birthdate } = getUserSessionData();
+const userData = getUserSessionData();
+console.log(userData);
 
 const ProfilePage = () => {
   const main = document.querySelector('main');
@@ -11,9 +12,9 @@ const ProfilePage = () => {
      <div class="row justify-content-center">
          <div class="profile">
              <h1 class=text-center>Profile</h1>
-             <h2 class=text-center>${username}</h2>
-             <h3 class=text-center>Email: ${email}</h3>
-             <h3 class="text-center">date de naissance: ${birthdate}</3>
+             <h2 class=text-center>${userData.username}</h2>
+             <h3 class=text-center>Email: ${userData.email}</h3>
+             <h3 class="text-center">date de naissance: ${userData.birthdate}</3>
          </div>
      </div>
     </div>
